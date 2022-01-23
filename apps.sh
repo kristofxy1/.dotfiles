a419c87ee1ce0256be36935c25150bcfb7093881
Krištof Bučar <kristofxy1@gmail.com>
2022-01-23 15:33:58 +0100

    edited for installing apps and nvim configure

--- apps.sh
+++ apps.sh
@@ -1,8 +1,15 @@
-sudo apt update
-sudo apt install zsh
-sudo apt install neovim
-sudo apt install ranger
+# ~/.dotfiles/install
 
-sudo pacman -Sy zsh
-sudo pacman -Sy ranger
-sudo pacman -Sy neovim
+sudo apt update
+sudo apt install -y zsh
+sudo apt install -y neovim
+sudo apt install -y ranger
+
+sudo pacman -Sy --noconfirm zsh
+sudo pacman -Sy --noconfirm ranger
+sudo pacman -Sy --noconfirm neovim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+chsh -s `which zsh`
