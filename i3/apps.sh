19183bd2403298ed8d7141303a097071f3b17edb
kristofxy1 <kristofxy1@gmail.com>
2022-02-22 10:09:36 +0100

    added apps.sh for installing dependencies for i3

---
+++ i3/apps.sh
@@ -0,0 +1,39 @@
+# adding ppa
+sudo add-apt-repository ppa:regolith-linux/release
+sudo add-apt-repository ppa:aslatter/ppa
+
+# update repositorys
+sudo apt-get update
+
+# installing - normal
+# sudo apt install flameshot
+# sudo apt install i3lock 
+# sudo apt install rofi
+# sudo apt install i3status 
+# sudo apt install clipit 
+# sudo apt install blueman 
+# sudo apt install network-manager-gnome 
+# sudo apt install xfce4-power-manager 
+
+# installing - normal
+sudo apt install flameshot i3lock rofi i3status clipit blueman network-manager-gnome xfce4-power-manager 
+
+# installilng other programs
+# sudo apt install xbacklight
+# sudo apt install pactl
+# sudo apt install playerctl 
+# sudo apt install systemctl 
+# sudo apt install xcape 
+# sudo apt install setxbmap 
+# sudo apt install nmuti 
+# sudo apt install pavucontrol 
+# sudo apt install blueberry
+
+# installilng other programs
+sudo apt install xbacklight pactl playerctl systemctl xcape setxbmap nmuti pavucontrol blueberry
+
+# installing with added ppas
+sudo apt install i3-gaps
+sudo apt install alacritty
+
+
